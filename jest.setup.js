90b9541dab1d4ec8e3816725a03d4edb34b44039
Nathan <NathHorrigan@gmail.com>
2020-11-11 09:27:32 +0000

    Fix tests with mock

--- jest.setup.js
+++ jest.setup.js
@@ -11,3 +11,13 @@ jest.mock('@react-navigation/native', () => {
     }),
   }
 })
+
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist')
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  }
+})
