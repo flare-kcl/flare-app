4fea9568d70e40def26711701883f9bb48e57357
Nathan <NathHorrigan@gmail.com>
2020-11-24 10:53:07 +0000

    Fix bad rebase

--- src/screens/FearConditioningTrialScreen.tsx
+++ src/screens/FearConditioningTrialScreen.tsx
@@ -35,76 +35,71 @@ export type FearConditioningTrialResponse = {
   volume: number
 }
 
-export const FearConditioningTrialScreen: ModuleScreen<FearConditioningTrialScreenParams> = memo(({
-  route,
-}) => {
-  const {
-    stimulusImage,
-    contextImage,
-    trialLength,
-    reinforced,
-    ratingDelay,
-    onTrialEnd,
-    trialDelay,
-  } = route.params
-  const dispatch = useDispatch()
-  const [showTrial, setShowTrial] = useState<boolean>(false)
-  const [rating, setRating] = useState<number>()
-  const [showScale, setShowScale] = useState(false)
+export const FearConditioningTrialScreen: ModuleScreen<FearConditioningTrialScreenParams> = memo(
+  ({ route }) => {
+    const {
+      stimulusImage,
+      contextImage,
+      trialLength,
+      reinforced,
+      ratingDelay,
+      onTrialEnd,
+      trialDelay,
+    } = route.params
+    const dispatch = useDispatch()
+    const [showTrial, setShowTrial] = useState<boolean>(false)
+    const [rating, setRating] = useState<number>()
+    const [showScale, setShowScale] = useState(false)
 
-  // Keep reference of timers
-  const soundRef = useRef<Audio.Sound>()
-  const startTimeRef = useRef<number>()
-  const reactionTimeRef = useRef<number>()
-  const endTimerRef = useRef<any>()
-  const soundTimerRef = useRef<any>()
-  const scaleTimerRef = useRef<any>()
-  const soundSensorListener = useRef<EmitterSubscription>()
+    // Keep reference of timers
+    const soundRef = useRef<Audio.Sound>()
+    const startTimeRef = useRef<number>()
+    const reactionTimeRef = useRef<number>()
+    const endTimerRef = useRef<any>()
+    const soundTimerRef = useRef<any>()
+    const scaleTimerRef = useRef<any>()
+    const soundSensorListener = useRef<EmitterSubscription>()
 
-  const setupSound = async () => {
-    try {
-      const { sound } = await Audio.Sound.createAsync(
-        require('../assets/sounds/ding.wav'),
-        {
-          shouldPlay: false,
-        },
-      )
-
-      // Assign sound object to ref
-      soundRef.current = sound
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  const playSound = async () => {
-    try {
-      await soundRef.current?.playAsync()
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    // Setup sound object before setting timers
-    setupSound()
-
-    // Setup Volume Listening
-    soundSensorListener.current = AudioSensor.addVolumeListener(
-      (volume: number) => {
-        dispatch(
-          recordEvent({
-            eventType: 'VolumeChange',
-            value: String(volume),
-            recorded: Date.now(),
-          }),
+    const setupSound = async () => {
+      try {
+        const { sound } = await Audio.Sound.createAsync(
+          require('../assets/sounds/ding.wav'),
+          {
+            shouldPlay: false,
+          },
         )
-      },
-    )
+
+        // Assign sound object to ref
+        soundRef.current = sound
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    const playSound = async () => {
+      try {
+        await soundRef.current?.playAsync()
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
     useEffect(() => {
       // Setup sound object before setting timers
       setupSound()
+
+      // Setup Volume Listening
+      soundSensorListener.current = AudioSensor.addVolumeListener(
+        (volume: number) => {
+          dispatch(
+            recordEvent({
+              eventType: 'VolumeChange',
+              value: String(volume),
+              recorded: Date.now(),
+            }),
+          )
+        },
+      )
 
       setTimeout(() => {
         // Set timer for end of trial
@@ -143,80 +138,57 @@ export const FearConditioningTrialScreen: ModuleScreen<FearConditioningTrialScre
         // Delete sound object
         soundRef.current?.unloadAsync()
 
+        // Disconnect volume listener
+        soundSensorListener.current.remove()
+
         // Delete timers
         clearTimeout(endTimerRef.current)
         clearTimeout(soundTimerRef.current)
         clearTimeout(scaleTimerRef.current)
       }
+    })
 
-      // Set timer for scale reveal
-      scaleTimerRef.current = setTimeout(() => {
-        // Show scale
-        setShowScale(true)
-      }, ratingDelay)
-
-      // Show the trial
-      setShowTrial(true)
-
-      // Mark the start time
-      startTimeRef.current = Date.now()
-
-    }, trialDelay ?? 0)
-
-    return () => {
-      // Delete sound object
-      soundRef.current?.unloadAsync()
-
-      // Disconnect volume listener
-      soundSensorListener.current.remove()
-
-      // Delete timers
-      clearTimeout(endTimerRef.current)
-      clearTimeout(soundTimerRef.current)
-      clearTimeout(scaleTimerRef.current)
-    }
-  })
-
-  return (
-    <>
-      {/* Show marker when delaying trial */}
-      {showTrial === false && (
-        <Box
-          flex={1}
-          width="100%"
-          position="absolute"
-          top={0}
-          pt={24}
-          alignItems="center"
-        >
-          <Text fontWeight="bold" fontSize={120}>
-            +
-          </Text>
-        </Box>
-      )}
-
-      {/* Set opacity to zero during inter-trial delay to enable loading of images */}
-      <Box flex={1} opacity={showTrial ? 1 : 0}>
-        <Box height="70%">
-          <TrialImageStack
-            contextImage={contextImage}
-            stimulusImage={stimulusImage}
-          />
-        </Box>
-        {showScale && (
-          <TrialRatingScale
-            onChange={(value) => {
-              // Record time to rate
-              reactionTimeRef.current = Date.now()
-              // Update state
-              setRating(value)
-            }}
-          />
+    return (
+      <>
+        {/* Show marker when delaying trial */}
+        {showTrial === false && (
+          <Box
+            flex={1}
+            width="100%"
+            position="absolute"
+            top={0}
+            pt={24}
+            alignItems="center"
+          >
+            <Text fontWeight="bold" fontSize={120}>
+              +
+            </Text>
+          </Box>
         )}
-      </Box>
-    </>
-  )
-})
+
+        {/* Set opacity to zero during inter-trial delay to enable loading of images */}
+        <Box flex={1} opacity={showTrial ? 1 : 0}>
+          <Box height="70%">
+            <TrialImageStack
+              contextImage={contextImage}
+              stimulusImage={stimulusImage}
+            />
+          </Box>
+          {showScale && (
+            <TrialRatingScale
+              onChange={(value) => {
+                // Record time to rate
+                reactionTimeRef.current = Date.now()
+                // Update state
+                setRating(value)
+              }}
+            />
+          )}
+        </Box>
+      </>
+    )
+  },
+)
 
 // Set the screen ID for navigator
 FearConditioningTrialScreen.screenID = 'trial'
