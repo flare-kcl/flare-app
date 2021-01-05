e84004aec6c30910043ed45546ab1a18c451c7f6
Nathan Horrigan <NathHorrigan@gmail.com>
2021-01-05 13:58:05 +0000

    Quick Tweaks

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -223,7 +223,7 @@ async function loginWithID(participantID: string, dispatch) {
     }
 
     // Check id user is locked out
-    if (experimentApiData.participant_started_at !== null) {
+    if (experimentApiData.participant_started_at != null) {
       return Promise.reject(
         'This particpant id has already been used, Please contact your research contact.',
       )
