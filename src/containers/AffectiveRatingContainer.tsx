fa300497ec5a4905189f249e717a2e17503adb1e
Nathan Horrigan <nathhorrigan@gmail.com>
2021-01-12 15:49:00 +0000

    Abstract Affective Rating container to show all stimuli for one module (#67)
    * Abstract Affective Rating container to show all stimuli for one module
    
    * Update shuffle imports

--- src/containers/AffectiveRatingContainer.tsx
+++ src/containers/AffectiveRatingContainer.tsx
@@ -1,43 +1,86 @@
-import { ExperimentModule } from './ExperimentContainer'
+import { ExperimentModule, VisualStimuli } from './ExperimentContainer'
 import { AffectiveRatingScreen } from '@screens'
-import { ImageSourcePropType } from 'react-native'
+import shuffle from 'lodash/shuffle'
+import { useEffect } from 'react'
+
+export type AffectiveRatingResponse = VisualStimuli & {
+  rating?: number
+}
 
 export type AffectiveRatingModuleDefinition = {
-  stimulus: string
   question: string
+  generalisationStimuliEnabled: boolean
   ratingScaleAnchorLabelLeft: string
   ratingScaleAnchorLabelCenter: string
   ratingScaleAnchorLabelRight: string
 }
 
 export type AffectiveRatingModuleState = AffectiveRatingModuleDefinition & {
-  rating?: number
+  stimuli?: AffectiveRatingResponse[]
+  currentStimuliIndex?: number
 }
 
 export const AffectiveRatingContainer: ExperimentModule<AffectiveRatingModuleState> = ({
   experiment,
   module: mod,
+  updateModule,
   onModuleComplete,
 }) => {
-  const stimulusImage: ImageSourcePropType =
-    mod.stimulus === 'csa' || mod.stimulus === 'csb'
-      ? experiment.definition.conditionalStimuli.find(
-          (cs) => cs.label == mod.stimulus,
-        )?.image
-      : experiment.definition.generalisationStimuli.find(
-          (gs) => gs.label == mod.stimulus,
-        )?.image
+  useEffect(() => {
+    if (mod.stimuli === undefined) {
+      updateModule({
+        currentStimuliIndex: 0,
+        stimuli: shuffle(
+          mod.generalisationStimuliEnabled
+            ? [
+                ...experiment.definition.conditionalStimuli,
+                ...experiment.definition.generalisationStimuli,
+              ]
+            : experiment.definition.conditionalStimuli,
+        ),
+      })
+    }
+  }, [])
+
+  // If no stimuli then halt...
+  if (mod.stimuli === undefined) return null
+
+  // Get the current stimuli
+  const stimulus: AffectiveRatingResponse = mod.stimuli[mod.currentStimuliIndex]
 
   function onRatingComplete(rating: number) {
-    // Save rating response & Mark module finished
-    onModuleComplete({ rating })
+    // Update array with response
+    const updatedStimuli = mod.stimuli.map(
+      (stimuli: AffectiveRatingResponse, index) => {
+        if (index === mod.currentStimuliIndex) {
+          return {
+            ...stimuli,
+            rating,
+          }
+        }
+
+        return stimuli
+      },
+    )
+
+    // Increment until the end of stimuli
+    if (mod.currentStimuliIndex === mod.stimuli.length - 1) {
+      // Save rating response & Mark module finished
+      onModuleComplete({ stimuli: updatedStimuli })
+    } else {
+      // Save rating response
+      updateModule({
+        stimuli: updatedStimuli,
+        currentStimuliIndex: mod.currentStimuliIndex + 1,
+      })
+    }
   }
 
   return (
     <AffectiveRatingScreen
+      key={`af-trial-${stimulus.label}`}
       question={mod.question}
-      stimulus={mod.stimulus}
-      stimulusImage={stimulusImage}
+      stimulusImage={stimulus.image}
       ratingScaleAnchorLabelCenter={mod.ratingScaleAnchorLabelCenter}
       ratingScaleAnchorLabelLeft={mod.ratingScaleAnchorLabelLeft}
       ratingScaleAnchorLabelRight={mod.ratingScaleAnchorLabelRight}
