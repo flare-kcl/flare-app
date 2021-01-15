70b6d2043ffcd3de07f70dcfdf2e17a47fcd0d6c
Nathan Horrigan <NathHorrigan@gmail.com>
2021-01-15 09:10:31 +0000

    Tweaks

--- src/screens/BreakEndScreen.tsx
+++ src/screens/BreakEndScreen.tsx
@@ -1,3 +1,4 @@
+import { ScrollView } from 'react-native'
 import { Box, Text, Button, SafeAreaView } from '@components'
 
 type BreakEndScreenProps = {
@@ -23,58 +24,64 @@ export const BreakEndScreen: React.FunctionComponent<BreakEndScreenProps> = ({
 }) => {
 
   return (
-    <SafeAreaView flex={1}>
-      <Box
-        flex={1}
-        alignItems="center"
-        justifyContent="flex-start"
-        pt={{ s: 8, m: 12 }}
-        px={5}
-      >
-        <Text variant="instructionHeading" mb={10}>
-          {heading}
-        </Text>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <SafeAreaView flex={1}>
+          <Box
+            flex={1}
+            alignItems="center"
+            justifyContent="flex-start"
+            pt={{ s: 8, m: 12 }}
+            px={5}
+          >
+            { heading && (
+              <Text variant="instructionHeading" mb={10}>
+                {heading}
+              </Text>
+            )}
 
-        <Text variant="instructionDescription" mb={16} textAlign='left'>
-          {description}
-        </Text>
+            { description && (
+              <Text variant="instructionDescription" mb={16} textAlign='left'>
+                {description}
+              </Text>
+            )}
 
-        <Text variant="caption2" fontWeight='600' pb={2}>
-          Time Left:
-        </Text>
+            <Text variant="caption2" fontWeight='600' pb={2}>
+              Time Left:
+            </Text>
 
-        {timerText && (
-          <Text fontWeight='800' textAlign='center' fontSize={45}>
-            {timerText}
-          </Text>
-        )}
+            {timerText && (
+              <Text fontWeight='800' textAlign='center' fontSize={45}>
+                {timerText}
+              </Text>
+            )}
 
-        {extendedTimerText && (
-          <Text fontWeight='800' textAlign='center' fontSize={20}>
-            {extendedTimerText}
-          </Text>
-        )}
+            {extendedTimerText && (
+              <Text fontWeight='800' textAlign='center' fontSize={20}>
+                {extendedTimerText}
+              </Text>
+            )}
 
-        {eta && (
-          <Text variant="caption2" fontWeight='600' pt={10}>
-            ETA: {eta}
-          </Text>
-        )}
+            {eta && (
+              <Text variant="caption2" fontWeight='600' pt={10}>
+                ETA: {eta}
+              </Text>
+            )}
 
-        <Box flex={1} justifyContent="flex-end" pb={6}>
-          <Text variant="caption2" px={4} mb={3} textAlign="center">
-            {actionLabel}
-          </Text>
+            <Box flex={1} justifyContent="flex-end" pb={6}>
+              <Text variant="caption2" px={4} mb={3} textAlign="center">
+                {actionLabel}
+              </Text>
 
-          <Button
-            variant="primary"
-            label="Next"
-            onPress={onNext}
-            opacity={buttonDisabled ? 0.4 : 1}
-            disabled={buttonDisabled}
-          />
-        </Box>
-      </Box>
-    </SafeAreaView>
+              <Button
+                variant="primary"
+                label="Next"
+                onPress={onNext}
+                opacity={buttonDisabled ? 0.4 : 1}
+                disabled={buttonDisabled}
+              />
+            </Box>
+          </Box>
+        </SafeAreaView>
+    </ScrollView>
   )
 }
