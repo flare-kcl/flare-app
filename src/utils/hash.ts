5283287fdaaf41a6f68ae505bf73d8a91381811c
Nathan Horrigan <NathHorrigan@gmail.com>
2021-02-17 16:52:24 +0000

    Fix duplicate image caching

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,10 +1,48 @@
-// Utility method used to generate unqique strings
-// https://lowrey.me/implementing-javas-string-hashcode-in-javascript/
-export const hashCode = function (str: string) {
-  let hash = 0
-  for (let i = 0; i < str.length; i++) {
-    hash += Math.pow(str.charCodeAt(i) * 31, str.length - i)
-    hash = hash & hash // Convert to 32bit integer
+import random from 'lodash/random'
+
+export const hashCode = function (
+  str: string,
+  seed: number = random(0, 100, true),
+) {
+  var l = str.length,
+    h = seed ^ l,
+    i = 0,
+    k
+
+  while (l >= 4) {
+    k =
+      (str.charCodeAt(i) & 0xff) |
+      ((str.charCodeAt(++i) & 0xff) << 8) |
+      ((str.charCodeAt(++i) & 0xff) << 16) |
+      ((str.charCodeAt(++i) & 0xff) << 24)
+
+    k = (k & 0xffff) * 0x5bd1e995 + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16)
+    k ^= k >>> 24
+    k = (k & 0xffff) * 0x5bd1e995 + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16)
+
+    h =
+      ((h & 0xffff) * 0x5bd1e995 +
+        ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16)) ^
+      k
+
+    l -= 4
+    ++i
   }
-  return hash
+
+  switch (l) {
+    case 3:
+      h ^= (str.charCodeAt(i + 2) & 0xff) << 16
+    case 2:
+      h ^= (str.charCodeAt(i + 1) & 0xff) << 8
+    case 1:
+      h ^= str.charCodeAt(i) & 0xff
+      h =
+        (h & 0xffff) * 0x5bd1e995 + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16)
+  }
+
+  h ^= h >>> 13
+  h = (h & 0xffff) * 0x5bd1e995 + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16)
+  h ^= h >>> 15
+
+  return h >>> 0
 }
