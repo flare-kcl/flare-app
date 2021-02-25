41a24f5c7e88ff9d3d3f1b1c1165bb677cce23c9
Nathan Horrigan <NathHorrigan@gmail.com>
2021-02-25 11:36:04 +0000

    Add background color to Stepper

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -7,7 +7,7 @@ export const Stepper = ({
   color = 'purple',
 }) => {
   return (
-    <Box paddingTop={5}>
+    <Box paddingTop={5} backgroundColor='white'>
       {/* Top Dots */}
       <Box flexDirection="row" alignItems="center" justifyContent="center">
         {new Array(numberOfSteps).fill(0).map((_, index) => (
