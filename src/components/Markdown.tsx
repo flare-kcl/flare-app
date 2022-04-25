6fd2da1c6f40a393a4f48576521320c996d8e018
Mitchel Cabuloy <mixxorz@gmail.com>
2022-04-25 15:18:25 +0800

    Fix JS dependency issues

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,4 +1,4 @@
-import MarkdownDisplay from 'react-native-markdown-display'
+// import MarkdownDisplay from 'react-native-markdown-display'
 import * as WebBrowser from 'expo-web-browser'
 import { BoxProps } from '@shopify/restyle'
 
@@ -34,50 +34,54 @@ export const Markdown: React.FunctionComponent<MarkdownProps> = ({
 
   return (
     <Box {...props}>
-      <MarkdownDisplay
-        onLinkPress={onLinkPress}
-        style={{
-          body: {
-            fontFamily: 'Inter',
-            fontWeight: '500',
-            color: palette.darkGrey,
-            marginBottom: 10,
-          },
-          paragraph: {
-            marginBottom: 10,
-            fontSize: 16,
-            color: theme.colors.darkGrey,
-            fontWeight: '500',
-          },
-          list_item: {
-            marginBottom: 20,
-          },
-          link: {
-            color: palette.purple,
-          },
-          heading1: {
-            fontFamily: 'Inter',
-            fontSize: 30,
-            fontWeight: 'bold',
-            color: palette.darkGrey,
-            alignSelf: 'flex-start',
-            marginBottom: 10,
-          },
-          heading2: {
-            fontSize: 22,
-            fontFamily: 'Inter',
-            fontWeight: 'bold',
-            width: '100%',
-          },
-          heading3: {
-            fontFamily: 'Inter',
-            fontSize: 20,
-            fontWeight: '600',
-          },
-        }}
-      >
-        {children}
-      </MarkdownDisplay>
+      {children}
+      {/*
+
+      // <MarkdownDisplay
+      //   onLinkPress={onLinkPress}
+      //   style={{
+      //     body: {
+      //       fontFamily: 'Inter',
+      //       fontWeight: '500',
+      //       color: palette.darkGrey,
+      //       marginBottom: 10,
+      //     },
+      //     paragraph: {
+      //       marginBottom: 10,
+      //       fontSize: 16,
+      //       color: theme.colors.darkGrey,
+      //       fontWeight: '500',
+      //     },
+      //     list_item: {
+      //       marginBottom: 20,
+      //     },
+      //     link: {
+      //       color: palette.purple,
+      //     },
+      //     heading1: {
+      //       fontFamily: 'Inter',
+      //       fontSize: 30,
+      //       fontWeight: 'bold',
+      //       color: palette.darkGrey,
+      //       alignSelf: 'flex-start',
+      //       marginBottom: 10,
+      //     },
+      //     heading2: {
+      //       fontSize: 22,
+      //       fontFamily: 'Inter',
+      //       fontWeight: 'bold',
+      //       width: '100%',
+      //     },
+      //     heading3: {
+      //       fontFamily: 'Inter',
+      //       fontSize: 20,
+      //       fontWeight: '600',
+      //     },
+      //   }}
+      // >
+      //   {children}
+      // </MarkdownDisplay>
+        */}
     </Box>
   )
 }
