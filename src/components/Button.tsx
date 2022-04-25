f64ac049113661ee30fbf6c3f99fe6fd4057d4ea
Mitchel Cabuloy <mixxorz@gmail.com>
2022-04-25 18:20:48 +0800

    Fix Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,9 @@
-import { TouchableOpacity, Animated, TouchableOpacityProps } from 'react-native'
+import {
+  TouchableOpacity,
+  Animated,
+  TouchableOpacityProps,
+  View,
+} from 'react-native'
 import {
   useRestyle,
   spacing,
@@ -11,44 +16,51 @@ import {
   AllProps,
   BackgroundColorProps,
   createVariant,
+  composeRestyleFunctions,
+  createRestyleComponent,
 } from '@shopify/restyle'
 
 import { Text } from './Text'
 import { Theme } from '@utils/theme'
 import { Box } from './Box'
 
-const restyleFunctions = [
+type RestyleProps = SpacingProps<Theme> &
+  BorderProps<Theme> &
+  BackgroundColorProps<Theme> &
+  VariantProps<Theme, 'buttonVariants'>
+
+const restyleFunctions = composeRestyleFunctions<Theme, RestyleProps>([
   spacing,
   border,
   backgroundColor,
   // Create new variant for themed buttons
-  createVariant({ themeKey: 'buttonVariants' }),
-]
-type Props = SpacingProps<Theme> &
-  BorderProps<Theme> &
-  BackgroundColorProps<Theme> &
-  TouchableOpacityProps &
-  VariantProps<Theme, 'buttonVariants'> &
-  AllProps<Theme> & {
-    // Custom Props...
-    testID?: string
-    label?: string
-    icon?: any
-    textProps?: TextProps<Theme>
-  }
+  createVariant<Theme>({ themeKey: 'buttonVariants' }),
+])
+
+type Props = RestyleProps & {
+  testID?: string
+  label?: string
+  icon?: any
+  textProps?: TextProps<Theme>
+  // Props for TouchableOpacity
+  onPress: () => void
+  flex?: number
+  activeOpacity?: number
+  disabled: boolean
+}
 
 export const Button = ({
   testID,
-  onPress,
   label,
   icon: Icon,
+  onPress,
   flex = 0,
   activeOpacity = 0.8,
   textProps,
   disabled,
   ...rest
 }: Props) => {
-  const props = useRestyle(restyleFunctions, rest)
+  const { style, ...props } = useRestyle(restyleFunctions, rest)
 
   return (
     <TouchableOpacity
