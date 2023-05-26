9ab3d3cf9b6012861f72db7247c829f2058dbff2
Mitchel Cabuloy <mixxorz@gmail.com>
2023-05-27 01:07:24 +0800

    Add docs to AudioSensor

--- src/utils/AudioSensor.ts
+++ src/utils/AudioSensor.ts
@@ -4,7 +4,14 @@ import { NativeEventEmitter, NativeModules } from 'react-native'
 
 const deviceInfoEmitter = new NativeEventEmitter(NativeModules.RNDeviceInfo)
 
-// Exposed class used by application code
+/**
+ * AudioSensor
+ *
+ * This class used to be a wrapper around our custom native AudioSensor module.
+ *
+ * The custom native module was removed in favor of using the react-native-volume-manager
+ * and react-native-device-info modules.
+ */
 export default class AudioSensor {
   private static currentVolume: number
   private static headphoneConnected: boolean
@@ -19,6 +26,14 @@ export default class AudioSensor {
     return DeviceInfo.isHeadphonesConnected()
   }
 
+  /**
+   * @deprecated
+   *
+   * This method used to ensure that AVAudioSession is active on iOS.
+   *
+   * It is unclear why this was needed, but now that the custom native module
+   * is removed, this method is no longer needed.
+   */
   static async focus(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       resolve(true)
