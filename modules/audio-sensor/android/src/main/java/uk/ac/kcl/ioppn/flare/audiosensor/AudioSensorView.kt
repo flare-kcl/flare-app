dcda31242a01e88a5e9aa93e09eb0854f5d05c68
Mitchel Cabuloy <mixxorz@gmail.com>
2023-05-27 00:07:38 +0800

    Remove audio-sensor Expo module

--- modules/audio-sensor/android/src/main/java/uk/ac/kcl/ioppn/flare/audiosensor/AudioSensorView.kt
+++
@@ -1,7 +0,0 @@
-package uk.ac.kcl.ioppn.flare.audiosensor
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class AudioSensorView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
